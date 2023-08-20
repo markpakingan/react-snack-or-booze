b352562edf4b8df1b10769249557b0f01b16f82a
markpakingan <116990353+markpakingan@users.noreply.github.com>
2023-08-19 19:20:14 -0700

    added drink menu nav bar

---
+++ src/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./NavBar.css";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "reactstrap";
+
+function NavBar() {
+  return (
+    <div>
+      <Navbar expand="md">
+        <NavLink exact to="/" className="navbar-brand">
+          Snack or Booze
+        </NavLink>
+
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink to="/snacks">Snacks </NavLink>
+          </NavItem>
+        </Nav>
+
+        <Nav>
+          <NavItem>
+            <NavLink to="/drinks">Drinks</NavLink>
+          </NavItem>
+        </Nav>
+
+      </Navbar>
+
+    </div>
+  );
+}
+
+export default NavBar;
