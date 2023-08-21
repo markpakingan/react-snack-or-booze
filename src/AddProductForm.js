9fb1ef1103a49904e4840d14f8db6bb9395ec9e7
markpakingan <116990353+markpakingan@users.noreply.github.com>
2023-08-20 20:18:33 -0700

    fixed the form type issue

--- src/AddProductForm.js
+++ src/AddProductForm.js
@@ -4,25 +4,56 @@ import { Button } from "reactstrap";
 
 const AddProductForm= () => {
 
-    const [productName, setProductName] = useState();
+
+    const INITIAL_STATE = {
+        name: '',
+        category: ''
+    }
+
+    const [formData, setFormData] = useState(INITIAL_STATE);
+
 
     const handleChange = (e) => {
-        setProductName(e.target.value)}
+        const {name, value} = e.target;
+        setFormData(formData => ({
+            ...formData, 
+            [name]: value
+        }))
+    }
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`The productname is ${productName}`)
+        alert(`The productname is ${formData.name} the category is ${formData.category}`)
     }
     
 
     return(
         <form>
-            <label htmlFor="productname"></label>
+            <label htmlFor="product-name">Product Name:</label>
             <input 
-            type="text" placeholder="Add product Name" value={productName} onChange={handleChange}/>
+            id="product-name"
+            name="name"
+            type="text" 
+            placeholder="Add product Name" 
+            value={formData.name} 
+            onChange={handleChange}/>
+            
+            <label htmlFor="category-name"></label>
+            <input 
+            id="category-name"
+            name="category"
+            type="text" 
+            placeholder="Add Category Name" 
+            value={formData.category} 
+            onChange={handleChange}/>
+
+
             <Button onClick={handleSubmit}>Add Product</Button>
         </form>
+
+
+
     )
 }
 
